1be8b676516d103d0c18bcda061da9b69bd6b888
MATHILDES-SURFA\mathi <mgaden15@student.aau.dk>
2018-05-07 14:21:12 +0200

    rootLayout - main referencer fucker

---
+++ src/main/java/controller/CreateAsthmaAppUserCtrl.java
@@ -0,0 +1,36 @@
+package main.controller;
+
+import javafx.fxml.FXML;
+
+
+public class CreateAsthmaAppUserCtrl {
+
+    // Reference to the main application. - Denne var i AddressApp men er ikke helt sikke på om vi skal bruge den.
+   // private main main;
+
+    /**
+     * The constructor.
+     * The constructor is called before the initialize() method.
+     */
+    public CreateAsthmaAppUserCtrl() {
+    }
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+
+    }
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     *
+     * @param main
+     */
+   // public void setMain(main main) {
+     //   this.main = main;
+
+   // }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,17 +1,69 @@
 import db_And_FHIR.dbControl;
 import javafx.application.Application;
 import db_And_FHIR.dbControl;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class main extends Application {
+
     private Stage primaryStage;
+    private BorderPane rootLayout;
     private java.sql.Connection con;
     dbControl myDBClass = new dbControl();
 
+    /**
+     * Constructor
+     */
+    public main() {
+        //
+    }
+
     @Override
     public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AstmaAppSystem");
+
+
     con = myDBClass.connect();
+
+        //isRegistered = DBCtrl.requestIsRegistered();   - Her skal vi indhente isRegistered fra DB og have en if-sætning.
+
+        //if(isRegistered){
+        initRootLayout(); //initiate root layout
+
+
+        // buildPatient();  //Her skal vi kalde vores funktioner til at bygge vores modeller
+        //showPerson(); //Her skal vi kalde vores funktioner til at vise
+
+        //}
+        //else{
+        //showCreateAsthmaAppUser();
+        //}
+
     }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(main.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
