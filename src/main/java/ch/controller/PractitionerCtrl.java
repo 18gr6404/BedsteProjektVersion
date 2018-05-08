051989fdf61faa1cca9e313b6e68e2f8586254c7
Nanna <nskogb15@student.aau.dk>
2018-05-08 16:17:48 +0200

    Tilføjet PractitionerView og linket til PractitionerCtrl

--- src/main/java/ch/controller/PractitionerCtrl.java
+++ src/main/java/ch/controller/PractitionerCtrl.java
@@ -1,4 +1,40 @@
 package main.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class PractitionerCtrl {
+
+    @FXML
+    private Label practitionerNameLabel;
+
+    // Reference to the main application. - Denne var i AddressApp men er ikke helt sikke på om vi skal bruge den.
+    // private main main;
+
+    /**
+     * The constructor.
+     * The constructor is called before the initialize() method.
+     */
+    public PatientCtrl() {
+    }
+
+    /**
+     * Initializes the ch.controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+
+    }
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     *
+     * @param main
+     */
+    // public void setmain(main main) {
+    //   this.main = main;
+
+    // }
+
 }
