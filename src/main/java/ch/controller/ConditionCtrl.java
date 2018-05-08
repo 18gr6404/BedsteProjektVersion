6155f56811da24dac2be68b12719106c72810ff6
XeniaUth <xuth15@student.aau.dk>
2018-05-08 16:38:33 +0200

    Oprettede Condition FXML

--- src/main/java/ch/controller/ConditionCtrl.java
+++ src/main/java/ch/controller/ConditionCtrl.java
@@ -1,4 +1,11 @@
 package main.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
 public class ConditionCtrl {
+    @FXML
+    private ListView komorbiditetList;
+
 }
